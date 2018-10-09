c4d5c5bdf6be5d3eba6a51e335eb7bb21f4a903c
Somant <somant@gmail.com>
2018-10-10 04:48:44 +0530

    add mlab

--- config/config.js
+++ config/config.js
@@ -4,3 +4,7 @@ if(run == 'local') {
     process.env.PORT = 3000
     process.env.MONGODB_URL = 'mongodb://127.0.0.1/Chat-App'
 }
+
+else{
+    var MONGODB_URI="mongodb://<Somant>:<1234>@ds251849.mlab.com:51849/mongoose-db";
+}
